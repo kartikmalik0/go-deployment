ece55873f17996808d788cdec67d3c5aa941797d
Kartik Malik <kartik20044@gmail.com>
2026-06-11 17:12:27 +0530

    added v1

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello from Go server!")
+    fmt.Fprintln(w, "Hello from Go server V1!")
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
